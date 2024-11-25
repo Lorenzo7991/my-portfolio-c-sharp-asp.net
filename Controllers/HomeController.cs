bac918a8da3936974622ca190c20d47ac42058a8
Lorenzo <lorenzo1997chierisini@gmail.com>
2024-11-25 17:34:20 +0100

    Created first draft home controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace MyPortfolio.Controllers
 {
@@ -13,17 +9,19 @@ namespace MyPortfolio.Controllers
             return View();
         }
 
+        public ActionResult Projects()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult MyContacts()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
